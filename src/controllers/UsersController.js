b4c6240da04bf5410c48b6ac1a2fde13cc53c8cf
chetan <chetankharel7@gmail.com>
2021-03-22 21:18:33 +0545

    CI test

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,6 +6,10 @@ module.exports = {
       const users = await User.findAll({
         attributes: { exclude: ['password'] }
       })
+      
+      if (users == null)
+        res.status(404).json({ message: 'users not found', data: users })
+
       return res.status(200).json({ users })
     } catch (error) {
       return res.status(500).send(error.message)
