9c14d1af2ca2d8ac795684989c95c9d9a5d57325
chetan <chetankharel7@gmail.com>
2021-03-25 23:09:17 +0545

    wip:profile picture upload

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const multer = require('multer')
+const path = require('path')
 
 const app = express()
 
 app.use(logger('dev'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json())
 app.use(cors())
 
 require('./routes')(app)
 require('dotenv').config()
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.filename + '-' + Date().now() + path.extname(file.originalname))
+    }
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`🚀🚀 Server listening on: ${PORT}`))
